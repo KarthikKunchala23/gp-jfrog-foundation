4694ec5baf77f4032af36d37ce017845cd711ee9
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-13 21:28:44 +0530

    added eip and nat routes to subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -50,3 +50,44 @@ resource "aws_route_table" "gp-jfrog-private-rt" {
   }
 }
 
+### Create Route for public subnet to Internet Gateway ######
+resource "aws_route" "gp-jfrog-public-route" {
+  route_table_id = aws_route_table.gp-jfrog-public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gp-jfrog-igw.id
+}
+
+resource "aws_route_table_association" "gp-public-rt-assn" {
+  count = length(var.public_subnet_cidr)
+  subnet_id = aws_subnet.gp-jfrog-public-subnet[count.index].id
+  route_table_id = aws_route_table.gp-jfrog-public-rt.id
+}
+
+resource "aws_route_table_association" "gp-private-rt-assn" {
+  count = length(var.private_subnet_cidr)
+  subnet_id = aws_subnet.gp-jfrog-private-subnet[count.index].id
+  route_table_id = aws_route_table.gp-jfrog-private-rt.id
+}
+
+### Create NAT Gateway for private subnet to access Internet ######
+resource "aws_eip" "gp-jfrog-nat-eip" {
+  domain = "vpc"
+  tags = {
+    Name = "gp-jfrog-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "gp-jfrog-nat" {
+  allocation_id = aws_eip.gp-jfrog-nat-eip.id
+  subnet_id = aws_subnet.gp-jfrog-public-subnet[0].id
+  tags = {
+    Name = "gp-jfrog-nat"
+  }
+  depends_on = [aws_internet_gateway.gp-jfrog-igw]
+}
+
+resource "aws_route" "gp-jfrog-private-route" {
+  route_table_id = aws_route_table.gp-jfrog-private-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.gp-jfrog-nat.id
+}
