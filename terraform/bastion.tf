2d7592cc3251edd1939752c89c10299ce88fd05f
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-20 14:26:29 +0530

    changed key for bastion

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "gp-jfrog-bastion" {
     associate_public_ip_address = true
     vpc_security_group_ids = [ aws_security_group.gp-jfrog-bastion-sg.id ]
     iam_instance_profile = data.aws_iam_role.bastion_rds.name
+    key_name = "jfrog_vm"
 
     tags = {
       Name = "Bastion"
