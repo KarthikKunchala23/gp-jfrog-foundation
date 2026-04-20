cafcdf39c41b8e554ae29c88bc992b7243b91d46
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-20 13:32:50 +0530

    changed az

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "gp-jfrog-bastion" {
     subnet_id = aws_subnet.gp-jfrog-public-subnet[0].id
     associate_public_ip_address = true
     vpc_security_group_ids = [ aws_security_group.gp-jfrog-bastion-sg.id ]
-    iam_instance_profile = data.aws_iam_role.bastion_rds.arn
+    iam_instance_profile = data.aws_iam_role.bastion_rds.name
 
     tags = {
       Name = "Bastion"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "az" {
   description = "Availabilty zones for servers"
   type = list(string)
-  default = ["ap-south-1a", "ap-south-1c"]
+  default = ["ap-south-1b", "ap-south-1c"]
 }
 
 variable "vpc_cidr" {
