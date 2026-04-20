5aff9a50710a76fa5c6c8b81af84fb337781f10a
KarthikKunchala23 <karthikkunchala2398@gmail.com>
2026-04-20 18:27:54 +0530

    fixed launch template issue

--- terraform/launchtemplate.tf
+++ terraform/launchtemplate.tf
@@ -134,12 +134,12 @@ sudo mkdir -p $SEC_DIR
 sudo aws s3 cp s3://jfrog-keys-bucket/jfrog/join.key $SEC_DIR/join.key
 sudo aws s3 cp s3://jfrog-keys-bucket/jfrog/master.key $SEC_DIR/master.key
 
-chown artifactory:artifactory $SEC_DIR/*
-chmod 600 $SEC_DIR/*
+sudo chown artifactory:artifactory $SEC_DIR/*
+sudo chmod 600 $SEC_DIR/*
 
 echo "Updating system.yaml with DB config..."
 
-cat > $SYSTEM_YAML <<EOL
+sudo cat > $SYSTEM_YAML <<EOL
 shared:
   database:
     type: postgresql
